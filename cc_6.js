d5e9cc816a88a1fc0ebd5e77102f2e055a73a339
Madisyn Notz <madisynnotz33@gmail.com>
2025-02-03 17:35:50 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -7,3 +7,13 @@ function calculateProfit(costPrice, sellingPrice, unitsSold) {
 // Test cases
 console.log(calculateProfit(20, 30, 100));  // Expected output: "Total Profit: $1000"
 console.log(calculateProfit(50, 70, 200));
+
+// Task 2: Function Expression
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    console.log(`Sales Tax: $${salesTax.toFixed(2)}`);
+};
+
+// Test cases
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7.00"
+calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50.00"
